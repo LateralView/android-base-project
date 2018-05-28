fb55866df192785cbe0825cf78d6ef1bf460d826
Juan Cruz Soler <juancruz.gsoler@gmail.com>
2018-05-28 19:56:49 -0300

    Supress checkstyle in User class

--- app/src/main/java/co/lateralview/myapp/domain/model/User.java
+++ app/src/main/java/co/lateralview/myapp/domain/model/User.java
@@ -2,9 +2,10 @@ package co.lateralview.myapp.domain.model;
 
 import java.io.Serializable;
 
-public final class User implements Serializable {
+@SuppressWarnings("checkstyle:hideutilityclassconstructor")
+public class User implements Serializable {
     public static final String SHARED_PREFERENCE_KEY = "co.lateralview.myapp.domain.model.User";
 
-    private User() {
+    public User() {
     }
 }
